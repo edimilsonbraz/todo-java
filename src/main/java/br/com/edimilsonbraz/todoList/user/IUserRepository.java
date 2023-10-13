82591abc5cb33e1cbc555caad99a0bb35d256dc9
edimilson braz <edimilson.gt8@gmail.com>
2023-10-13 09:07:34 -0300

    :tada: First commit

---
+++ src/main/java/br/com/edimilsonbraz/todoList/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package br.com.edimilsonbraz.todoList.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+    UserModel findByUsername(String username);
+}
